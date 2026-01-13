bb0e6ba5c64c64ea4bfe2e3d726f3c94439991ab
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-01-13 06:33:08 -0600

    LC-jump-game [Runtime: 15 ms; Memory: 20.6 MB]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -1,13 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums)==1:
-            return True
-        reach=[0]*len(nums)
-        reach[0]=1
-        for i in range(len(nums)):
-            if reach[i]:
-                for j in range(nums[i]):
-                    if (i+1+j)<len(nums):
-                        reach[i+1+j]=1
-        return True if reach[-1] else False
+        farthest_reach=0
+        n=len(nums)
+        for i in range(n):
+            if i>farthest_reach:
+                return False
+            farthest_reach=max(i+nums[i], farthest_reach)
+            if farthest_reach>=n-1:
+                return True
 
