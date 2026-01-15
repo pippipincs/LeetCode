323c9c7082261d857607ec0da33e6e1609d8589a
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-01-15 05:39:31 -0600

    LC-insert-delete-getrandom-o1 [Runtime: N/A; Memory: N/A]

---
+++ hash-table/insert-delete-getrandom-o1.py
@@ -0,0 +1,37 @@
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.arr=[]
+        self.map={}
+
+
+    def insert(self, val: int) -> bool:
+        if val in self.map:
+            return False
+        else:
+            self.arr.append(val)
+            self.map[val]=len(self.arr)-1
+            return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        else:
+            idx=self.map[val]
+            self.arr[idx]=self.arr[-1]
+            del self.map[val]
+            self.map[self.arr[-1]]=idx
+            self.arr.pop()
+            return True
+
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
