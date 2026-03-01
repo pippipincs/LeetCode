6f2d974751b82d470071246e4c22da61e9960881
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-02-28 23:32:49 -0600

    LC-jump-game [Runtime: 12 ms; Memory: 20.4 MB]

--- array/jump-game.py
+++ array/jump-game.py
@@ -1,11 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [0] * len(nums)
-        dp[-1] = 1
+        pos = len(nums) - 1
         for i in range(len(nums)-1, -1, -1):
-            for j in range(i+1, min((i + nums[i]+1), len(nums))):
-                if dp[j] == 1:
-                    dp[i] = 1
-                    break
-        return dp[0] == 1
-        
+            if i + nums[i] >= pos:
+                pos = i
+        return pos == 0
