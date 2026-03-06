eba381936eb72e59e46202869c46903d3ff932c4
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-03-05 18:22:34 -0600

    LC-gas-station [Runtime: 35 ms; Memory: 26 MB]

---
+++ greedy/gas-station.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total = 0
+        curr = 0
+        start = 0
+        for i in range(len(gas)):
+            total += gas[i] - cost[i]
+            curr += gas[i] -cost[i]
+
+            if curr < 0:
+                curr = 0
+                start = i +1
+        return start if total >= 0 else -1
