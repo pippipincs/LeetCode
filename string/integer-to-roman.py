9ac45d6d56a7b97659ba84f7cd21c040b7b5fcba
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-03-07 16:46:35 -0600

    LC-integer-to-roman [Runtime: 9 ms; Memory: 19.1 MB]

---
+++ string/integer-to-roman.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            ["M", 1000],
+            ["CM", 900],
+            ["D", 500],
+            ["CD", 400],
+            ["C", 100],
+            ["XC", 90],
+            ["L", 50],
+            ["XL", 40],
+            ["X", 10],
+            ["IX", 9],
+            ["V", 5],
+            ["IV", 4],
+            ["I", 1]
+        ]
+        res = []
+        for symbol, value in values:
+            count = num // value
+            num = num % value
+            for i in range(count):
+                res.append(symbol)
+        return "".join(res)
+
