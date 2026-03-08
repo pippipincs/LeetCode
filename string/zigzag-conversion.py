12e81cb48f4a737750b9b2537cfc556cfb30816c
PPiP <110551362+pippipincs@users.noreply.github.com>
2026-03-07 20:06:16 -0600

    LC-zigzag-conversion [Runtime: N/A; Memory: N/A]

---
+++ string/zigzag-conversion.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        zigzag = collections.defaultdict(list)
+        i = 0
+        change = 1
+        for c in s:
+            zigzag[i].append(c)
+            if i == 0:
+                change = 1
+            elif i == numRows - 1:
+                change = -1
+            i += change
+        res= ""
+        for i in range(numRows):
+            res += "".join(zigzag[i])
+        return res
