6b569154c215f62ac998db4663e0937a6dd4bf70
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-06-16 22:07:55 -0500

    LC-basic-calculator-ii [Runtime: N/A; Memory: N/A]

--- stack/basic-calculator-ii.py
+++ stack/basic-calculator-ii.py
@@ -1,21 +1,27 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
+        
         n = len(s)
         operations = "+"
         curr = 0
+        last = 0
+        res = 0
         for i, ch in enumerate(s):
             if ch.isdigit():
                 curr = curr * 10 + int(ch)
             if (not ch.isdigit() and ch != " ") or i == n - 1:
                 if operations == "+":
-                    stack.append(curr)
+                    
+                    res += last
+                    last = curr
                 if operations == "-":
-                    stack.append(-curr)
+                    res += last
+                    last = -curr
                 if operations == "*":
-                    stack.append(stack.pop() * curr)
+                    last = last * curr
                 if operations == "/":
-                    stack.append(int(stack.pop() / curr))
+                    last = int(last / curr)
                 operations = ch
                 curr = 0
-        return sum(stack)
+        res += last
+        return res
