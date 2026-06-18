050b7e6e1ea727c2e5ad849c2dad19889e48c4a9
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-06-17 20:52:24 -0500

    LC-minimum-changes-to-make-alternating-binary-string [Runtime: N/A; Memory: N/A]

---
+++ string/minimum-changes-to-make-alternating-binary-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        start0 = 0
+        for i, c in enumerate(s):
+            if i % 2 == 0:
+                if s[i] == "1":
+                    start0 += 1
+            else:
+                if s[i] == "0":
+                    start0 += 1
+        return min(start0, len(s) - start0)
