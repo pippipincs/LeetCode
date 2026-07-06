d7c4dbbcfacb21c4ee2836f0e16da9d16ad2e4c2
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-07-06 18:19:45 -0500

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- depth-first-search/course-schedule.py
+++ depth-first-search/course-schedule.py
@@ -1,27 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def dfs(stack, visit, i):
-            if i in stack:
-                return True
-            if i in visit:
-                return False
-            stack.add(i)
-            visit.add(i)
-            for nei in adj[i]:
-                if dfs(stack, visit, nei):
-                    return True
-            stack.remove(i)
-            return False
-
-        stack = set()
-        visit = set()
-
-        adj = [[] for _ in range(numCourses)]
-
-        for pre, crs in prerequisites:
-            adj[crs].append(pre)
-
-        for i in range(numCourses):
-            if dfs(stack, visit, i):
+        indegree = [0] * numCourses
+        g = collections.defaultdict(list)
+        for a, b in prerequisites:
+            g[b].append(a)
+            indegree[a] += 1
+        
+        queue = deque([i for i, v in enumerate(indegree) if v == 0])
+        while queue:
+            crs = queue.popleft()
+            for nextcrs in g[crs]:
+                indegree[nextcrs] -= 1
+                if indegree[nextcrs] == 0:
+                    queue.append(nextcrs)
+        for v in indegree:
+            if v != 0:
                 return False
         return True
+
+        
