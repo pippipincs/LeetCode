2b06fdd93cf197f18b1e645d5a960a98e0b6681b
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-07-12 10:52:13 -0500

    LC-minimum-number-of-arrows-to-burst-balloons [Runtime: N/A; Memory: N/A]

---
+++ greedy/minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key = lambda x : x[1])
+        arrows = 1
+        first_end = points[0][1]
+        for x_start, x_end in points[1:]:
+            if x_start > first_end:
+                arrows += 1
+                first_end = x_end
+        return arrows
+        
