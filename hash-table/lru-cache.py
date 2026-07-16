e7d2b472ed2441ae89014f820c31b21364c3d84f
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-07-15 20:50:07 -0500

    LC-lru-cache [Runtime: N/A; Memory: N/A]

--- hash-table/lru-cache.py
+++ hash-table/lru-cache.py
@@ -1,62 +1,56 @@
-class Node:
-    def __init__(self, key, value):
+class DoubleNode:
+    def __init__(self, key, val, prev, nextnode):
         self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-    
+        self.val = val
+        self.prev = prev
+        self.next = nextnode
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.mapping = {}
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.dict = {}
         self.capacity = capacity
-
+        self.count = 0
+        self.head = DoubleNode(0, 0, None, None)
+        self.tail = self.head
     def get(self, key: int) -> int:
-        if key not in self.mapping:
-            return -1
-        else:
-            node = self.mapping[key]
+        if key in self.dict:
+            node = self.dict[key]
             self.remove(node)
-            self.add(node)
-            return node.value
-
+            self.insert(node)
+            return node.val
+        else:
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.mapping:
-            node = self.mapping[key]
+        if key in self.dict:
+            node = self.dict[key]
+            node.val = value
             self.remove(node)
-            node.value = value
-            self.mapping[key] = node
-            self.add(node)
+            self.insert(node)
         else:
-            node = Node(key, value)
-            self.add(node)
-            self.mapping[key] = node
-            if len(self.mapping) > self.capacity:
-                first = self.head.next
-                k = first.key
-                self.remove(first)
-                del self.mapping[k]
-
-
-
+            node = DoubleNode(key, value, None, None)
+            self.dict[key] = node
+            if self.count >= self.capacity:
+                node_delete = self.head.next
+                self.remove(node_delete)
+                del self.dict[node_delete.key]
+            else:
+                self.count += 1
+            self.insert(node)
     def remove(self, node):
-        prevnode = node.prev
-        nextnode = node.next
-        prevnode.next = nextnode
-        nextnode.prev = prevnode
-        node.prev = None
-        node.next = None 
-    def add(self, node):
-        prevnode = self.tail.prev
-        prevnode.next = node
-        node.prev = prevnode
-        node.next = self.tail
-        self.tail.prev = node
+        if self.tail == node:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+    def insert(self, node):
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = self.tail.next
+
+                
+
 
 
 # Your LRUCache object will be instantiated and called as such:
