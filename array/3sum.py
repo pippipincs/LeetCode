bd93cc89b3a859da245c1e68f45817b317cfb0c0
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-07-29 22:23:51 -0500

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -1,34 +1,35 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        n = len(nums)
-        i = 0
         res = []
-        while i < n:
-            if nums[i] > 0:
-                break
-            first = nums[i]
-            target = 0 - first
-            l = i + 1
-            r = len(nums) - 1
-            while l < r :
-                if nums[l] + nums[r] == target:
-                    res.append([first, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
-                elif nums[l] + nums[r] < target:
-                    l += 1
-                    while l < r and nums[l] == nums[l - 1]:
-                            l += 1
-                elif nums[l] + nums[r] > target:
-                    r -= 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
+        i = 0
+        while i < len(nums):
+            num = nums[i]
+            target = 0 - num
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                s = nums[left] + nums[right]
+                if s == target:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    
+                elif s > target:
+                    right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                else:
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
             i += 1
-            while i < n and nums[i] == nums[i - 1]:
+            while i < len(nums) and nums[i] == nums[i - 1]:
                 i += 1
         return res
+                
+                
