b5dd9f2081ae4dedb335baab972422531bce2113
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-07-31 23:43:50 -0500

    LC-find-the-index-of-the-first-occurrence-in-a-string [Runtime: N/A; Memory: N/A]

--- string-matching/find-the-index-of-the-first-occurrence-in-a-string.py
+++ string-matching/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,45 +1,14 @@
 class Solution:
+    
     def strStr(self, haystack: str, needle: str) -> int:
-        
-        n = len(haystack)
-        m = len(needle)
-
-        if m > n :
-            return -1
-        ##find the lps of needle
-        lps = [0] * m
-        prev = 0
-        i = 1
-        while i < m :
-            if needle[prev] == needle[i]:
-                prev += 1
-                lps[i] = prev
-                i += 1
-                continue
-            elif prev == 0:
-                lps[i] = 0
-                i += 1
-                continue
+        occur=True
+        for i in range(len(haystack)):
+            for j in range(len(needle)):
+                if i+j>=len(haystack) or needle[j]!=haystack[i+j]:
+                    occur=False
+                    break
+            if occur:
+                return i
             else:
-                prev = lps[prev - 1] 
-                continue
-        ## compare needle with haystack
-        i = 0 # needle pointer
-        j = 0 # haystack pointer
-        while j < n:
-            if haystack[j] == needle[i]:
-                i += 1
-                j += 1
-                if i == m:
-                    return j - m
-            elif i != 0:
-                i = lps[i - 1]
-            else:
-                
-                j += 1
+                occur=True
         return -1
-
-
-
-
-            
