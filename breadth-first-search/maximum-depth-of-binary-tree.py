c9222f3b615f5ec5d857754087a760afc8106a10
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-08-08 23:44:31 -0500

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

--- breadth-first-search/maximum-depth-of-binary-tree.py
+++ breadth-first-search/maximum-depth-of-binary-tree.py
@@ -6,14 +6,8 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root == None:
+        if not root:
             return 0
-        if root.left == None and root.right == None:
-            return 1
-        elif root.left == None:
-            return self.maxDepth(root.right) + 1
-        elif root.right == None:
-            return self.maxDepth(root.left) + 1
-        else:
-            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
-        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return 1 + max(left, right)
