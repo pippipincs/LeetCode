319b20f68045aae3332c1a6345477294c1f32e04
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-08-12 21:55:38 -0500

    LC-count-complete-tree-nodes [Runtime: N/A; Memory: N/A]

--- bit-manipulation/count-complete-tree-nodes.py
+++ bit-manipulation/count-complete-tree-nodes.py
@@ -5,7 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def compute_depth(self, root):
+        d = 0
+        while root.left:
+            d += 1
+            root = root.left
+        return d
+    def exist(self, idx, d, root):
+        left = 0
+        right = 2 ** d - 1
+        for _ in range(d):
+            mid = (left + right) // 2
+            if idx <= mid:
+                root = root.left
+                right = mid
+            else:
+                root = root.right
+                left = mid + 1
+        return True if root else False
+    
+
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+        d = self.compute_depth(root)
+        left, right = 0, 2 ** d - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if self.exist(mid, d, root):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return 2 ** d - 1 + left
