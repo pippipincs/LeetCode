abbb74472e56fa8b73faa147cf8b8dda452ace9b
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-08-15 15:43:03 -0500

    LC-binary-tree-paths [Runtime: N/A; Memory: N/A]

---
+++ string/binary-tree-paths.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        stack = []
+        stack.append((root, str(root.val)))
+        paths = []
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            elif node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            elif node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        return paths
