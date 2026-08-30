8a4333b01c736c8ad2328fe0368d496629253b64
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-08-30 13:20:16 -0500

    LC-lemonade-change [Runtime: N/A; Memory: N/A]

---
+++ greedy/lemonade-change.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        collected = collections.defaultdict(int)
+        for bill in bills:
+            if bill == 5 or bill == 10:
+                collected[bill] += 1
+            if bill == 10:
+                if collected[5] > 0:
+                    collected[5] -= 1
+                else:
+                    return False
+            if bill == 20:
+                if collected[5] > 0 and collected[10] > 0:
+                    collected[5] -= 1
+                    collected[10] -= 1
+                else:
+                    return False
+        return True
