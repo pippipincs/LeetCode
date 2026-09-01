c18dda2c5bab9ddab806e5c58eac4f13fa91c69f
Agness Pei <110551362+pippipincs@users.noreply.github.com>
2026-09-01 14:07:44 -0500

    LC-candy [Runtime: N/A; Memory: N/A]

--- array/candy.py
+++ array/candy.py
@@ -1,15 +1,10 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        left2right = [1] * len(ratings)
-        right2left = [1] * len(ratings)
+        res = [1] * len(ratings)
         for i in range(1, len(ratings)):
-            if ratings[i] > ratings[i-1]:
-                left2right[i] = left2right[i - 1] + 1
-        for j in range(len(ratings) -2, -1, -1):
-            if ratings[j] > ratings[j + 1]:
-                right2left[j] = right2left[j + 1] + 1
-        candies = 0
-        for i in range(len(ratings)):
-            candies += max(left2right[i], right2left[i])
-        return candies
-        
+            if ratings[i] > ratings[i - 1]:
+                res[i] = res[i - 1] + 1
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                res[i] = max(res[i + 1] + 1, res[i])
+        return sum(res)
